133537a25e7fe86a307fcdec7b851270654b2759
gonatae2-hash <gonatae2@gmail.com>
2026-02-13 17:03:58 +0900

    password 제거

--- api/test.py
+++ api/test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.users = {}
 app.id_count = 1 # new_user 등록시 카운트
 
-@app.route('/sign_up', methods=['POST'])
+@app.route('/sign_up', methods=['POST'])# POST = 데이터 새로 생성시
 def sign_up():
     new_user = request.json             # name=kim 입력 시 request.json 객체에 들어감.
                                         # new_user 변수에 대입.
@@ -12,12 +12,25 @@ def sign_up():
                                         # 그 값으로 app.id_count(1)를 넣음.
     app.users[app.id_count] = new_user  # id를 기준으로 유저 정보 저장.
     app.id_count = app.id_count + 1     # 새로운 유저를 위해 1씩 증가.
-    return jsonify(new_user)            # ame=kim 입력 시 id, name 출력.
+    return jsonify(new_user)            # name=kim 입력 시 id, name 출력.
 
-@app.route('/user/<int:user_id>', methods=['GET'])
+@app.route('/user/<int:user_id>', methods=['GET']) # GET = 저장된 데이터 조회
 def user(user_id):
     if user_id not in app.users:
         return '사용자가 존재하지 않습니다.', 400
     
     return jsonify(app.users[user_id])
 
+# 전체 유저 조회
+@app.route('/users', methods=['GET'])
+def users():
+    result = []
+    
+    for user in app.users.values(): #.values() 딕셔너리 안의 값만 가져옴.
+        remvpw_user = {key : value for key, value in user.items() if key != "password"}
+        # {만들형태  for 꺼낼값 in 반복대상  if 조건}
+        # password가 아닐 시 user.items()에서 key, value를 key : velue형태로
+        # repw_user 딕셔너리에 추가(user 한명씩 추가) 
+        result.append(remvpw_user) # repw_user를 result리스트에 추가.
+    return result
+                              
